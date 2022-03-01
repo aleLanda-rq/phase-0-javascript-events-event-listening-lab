a51741f11c27781d0ffe1e9061d0c9aeca7675fc
Alejandra <aleriquene@gmail.com>
2022-03-01 09:03:16 -0600

    commit tree 1c61482372d0a2f1f1f394ad5865ff8a880eb05c

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+
+// //good for one element
+// const input = document.getElementById('input')
+// .addEventListener('click',()=> alert('I was clicked!'));
+
+//multiple elements
+const input = document.getElementById('input');
+
+function clickAlert(){
+    alert('I was clicked');
+    //we would put the multiple elements here
+}
 function addingEventListener() {
+input.addEventListener('click', clickAlert);
+
 }
